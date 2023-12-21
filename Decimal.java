77f40047e0911c0c74a057916cdb2175358cde0b
sunil2xx3 <148536724+sunil2xx3@users.noreply.github.com>
2023-12-21 10:07:14 +0530

    Update Decimal.java

--- Decimal.java
+++ Decimal.java
@@ -1,33 +1,75 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.*;
 
-public class Decimal {
-    
-    public void removeDot(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("enter your decimal value.");
-        float f = scan.nextFloat();
-        String s = String.valueOf(f);
-        String ns="";
-        ArrayList <String> arr = new ArrayList<>(); 
-        
-        for (int i = 0; i <s.length()-1; i++) {
-            arr.add(Character.toString(s.charAt(i)));
-        }
-
-        // for (int j = arr.size()-1; j>0; j--) {
-        //     System.out.println(Integer.valueOf(arr.get(j)));
-        //     if(Integer.valueOf(arr.get(j))>5 ){
-        //         ns+=Integer.valueOf(arr.get(j-1))+1;
-        //     } 
-        // }
-        System.out.println(arr);
-
-
-
-    
-    }
+public class RoundingCalculator {
+    String[] arr;
     public static void main(String[] args) {
-        new Decimal().removeDot();
+        RoundingCalculator rc = new RoundingCalculator();
+        rc.roundingTheNumber();
+    }
+    public void roundingTheNumber(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the decimal value: ");
+        double num = input.nextDouble();
+        input.nextLine();
+
+        System.out.println("Enter the place you wants to round off: ");
+        String place = input.nextLine();
+
+        String str = "" + num;
+        arr = str.split("\\.");
+//        System.out.println(Arrays.toString(arr));
+
+        if (place.equals("one")){
+            onesPlace();
+        }
+        if (place.equals("ten")){
+            tenthPlace();
+        }
+        if (place.equals("hundred")){
+            hundredthPlace();
+        }
+    }
+    public void onesPlace(){
+        for (int i = 1; i < arr.length;i++) {
+            String numStr = arr[i];
+            int num1 = Integer.parseInt(String.valueOf(numStr.charAt(0)));
+            if (num1 < 5){
+                System.out.println(arr[0]);
+            }
+            if (num1 >= 5){
+                int real = Integer.parseInt(arr[0]);
+                System.out.println("Your number : " + (real + 1));
+            }
+        }
+    }
+    public void tenthPlace(){
+        for (int i = 1; i < arr.length;i++) {
+            String numStr = arr[i];
+            int num1 = Integer.parseInt(String.valueOf(numStr.charAt(1)));
+            if (num1 < 5){
+                System.out.println(arr[0] + "." + numStr.charAt(0));
+            }
+            if (num1 >= 5){
+                String decimal = arr[0] + "." + numStr.charAt(0);
+                double real = Double.parseDouble(decimal);
+                System.out.println("Your number : " + (real + 0.1));
+            }
+        }
+    }
+    public void hundredthPlace(){
+        for (int i = 1; i < arr.length;i++) {
+            String numStr = arr[i];
+            int num1 = Integer.parseInt(String.valueOf(numStr.charAt(2)));
+            if (num1 < 5){
+                System.out.println(arr[0] + "." + numStr.charAt(0) + numStr.charAt(1));
+            }
+            if (num1 >= 5){
+                String decimal = arr[0] + "." + numStr.charAt(0) + numStr.charAt(1);
+                double real = Double.parseDouble(decimal);
+                System.out.println("Your number : " + (real + 0.01));
+            }
+        }
     }
 }
